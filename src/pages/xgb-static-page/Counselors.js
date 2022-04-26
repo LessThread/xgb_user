968b09a3967c873dc174b5fd84e13861e1bbafe8
lyc_tju <allforone@tju.edu.cn>
2022-04-26 17:02:19 +0800

    add counselors

---
+++ src/pages/xgb-static-page/Counselors.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+	class Counselors extends Component {
+		render() {
+			return (
+				<div>
+                    <div id='Counselors_render'>
+                        for
+                    </div>
+				</div>
+			)
+		}
+	}
+	export default Counselors
